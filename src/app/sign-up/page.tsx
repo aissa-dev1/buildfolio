081636199836ddde9fb7d471c85167e73a566b7b
aissa-dev1 <aissadev1@gmail.com>
2025-01-01 23:37:58 +0100

    update sign up page

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,13 +1,39 @@
 import Bootstrap from "@/components/reusable/bootstrap";
 import Container from "@/components/reusable/container";
-import { H2 } from "@/components/ui/typography";
+import Button from "@/components/ui/button";
+import Input from "@/components/ui/input";
+import { H2, P } from "@/components/ui/typography";
+import Link from "next/link";
 
 export default function SignUp() {
   return (
     <Bootstrap>
-      <Container className="flex__center flex-col h-screen">
-        <H2 className="text-primary">Buildfolio</H2>
-        <H2>انشاء حساب</H2>
+      <Container className="flex__center h-screen">
+        <div className="flex flex-col gap-lg w-full text-center sm:w-[400px]">
+          <H2>
+            <span className="text-primary">Buildfolio</span> | انشاء حساب
+          </H2>
+          <form className="flex flex-col gap-md">
+            <Input type="text" placeholder="اسم المستخدم" required />
+            <Input type="email" placeholder="البريد الالكتروني" required />
+            <Input type="password" placeholder="كلمة المرور" required />
+            <Button type="submit">انشاء حساب</Button>
+          </form>
+          <div className="flex flex-col gap-md">
+            <P>
+              بالضغط على انشاء حساب فانت توافق على{" "}
+              <Link href="/terms" className="text-gray-foreground">
+                الشروط والاحكام
+              </Link>
+            </P>
+            <P>
+              لديك حساب بالفعل؟{" "}
+              <Link href="/login" className="text-gray-foreground">
+                تسجيل الدخول
+              </Link>
+            </P>
+          </div>
+        </div>
       </Container>
     </Bootstrap>
   );
