43fc310b651b9300116e2be9e7340472fc75d70d
aissa-dev1 <aissadev1@gmail.com>
2025-01-01 23:38:06 +0100

    create login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,31 @@
+import Bootstrap from "@/components/reusable/bootstrap";
+import Container from "@/components/reusable/container";
+import Button from "@/components/ui/button";
+import Input from "@/components/ui/input";
+import { H2, P } from "@/components/ui/typography";
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <Bootstrap>
+      <Container className="flex__center h-screen">
+        <div className="flex flex-col gap-lg w-full text-center sm:w-[400px]">
+          <H2>
+            <span className="text-primary">Buildfolio</span> | تسجيل الدخول
+          </H2>
+          <form className="flex flex-col gap-md">
+            <Input type="email" placeholder="البريد الالكتروني" required />
+            <Input type="password" placeholder="كلمة المرور" required />
+            <Button type="submit">تسجيل الدخول</Button>
+          </form>
+          <P>
+            ليس لديك حساب؟{" "}
+            <Link href="/sign-up" className="text-gray-foreground">
+              انشاء حساب
+            </Link>
+          </P>
+        </div>
+      </Container>
+    </Bootstrap>
+  );
+}
