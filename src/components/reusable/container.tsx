1c99970017a8318f024859149fb5bfafd5baa015
aissa-dev1 <aissadev1@gmail.com>
2025-01-01 23:40:24 +0100

    add horizontal padding in small devices

--- src/components/reusable/container.tsx
+++ src/components/reusable/container.tsx
@@ -4,5 +4,10 @@ import { ComponentProps } from "react";
 interface ContainerProps extends ComponentProps<"div"> {}
 
 export default function Container({ className, ...rest }: ContainerProps) {
-  return <div className={cn("container mx-auto", className)} {...rest} />;
+  return (
+    <div
+      className={cn("container mx-auto px-md sm:px-0", className)}
+      {...rest}
+    />
+  );
 }
