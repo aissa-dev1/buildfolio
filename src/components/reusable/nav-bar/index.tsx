732810fb7f19a21c9adf8f264903c1008f62b922
aissa-dev1 <aissadev1@gmail.com>
2025-01-01 23:36:07 +0100

    move nav bar component to its own folder

---
+++ src/components/reusable/nav-bar/index.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Container from "../container";
+import { P } from "@/components/ui/typography";
+import ThemeSwitchButton from "./theme-switch-button";
+
+export default function NavBar() {
+  return (
+    <nav className="h-[var(--navbar-height)] flex__center w-full bg-primary text-primary-foreground py-4 shadow-sm">
+      <Container className="flex justify-between items-center px-4">
+        <Link href="/">
+          <P size="lg" className="font-bold">
+            Buildfolio
+          </P>
+        </Link>
+        <div className="flex__center gap-md">
+          <Link href="/create">
+            <P className="hover:text-foreground">انشاء موقع</P>
+          </Link>
+          <Link href="/preview">
+            <P className="hover:text-foreground">معاينة</P>
+          </Link>
+          <ThemeSwitchButton />
+        </div>
+      </Container>
+    </nav>
+  );
+}
