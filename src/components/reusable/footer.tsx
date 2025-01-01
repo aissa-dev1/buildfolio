4160fbe5fadb393d1624720679dc1430be4417cc
aissa-dev1 <aissadev1@gmail.com>
2025-01-01 23:40:37 +0100

    update footer

--- src/components/reusable/footer.tsx
+++ src/components/reusable/footer.tsx
@@ -2,11 +2,18 @@ import { P } from "../ui/typography";
 import Container from "./container";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+  const createdYear = 2024;
+
   return (
-    <footer className="flex__center h-[var(--footer-height)] bg-secondary text-secondary-foreground py-4">
+    <footer className="flex__center h-[var(--footer-height)] bg-card text-card-foreground py-4">
       <Container className="text-center">
         <P size="md">
-          Buildfolio &copy; {new Date().getFullYear()} جميع الحقوق محفوظة.
+          Buildfolio &copy;{" "}
+          {currentYear === createdYear
+            ? createdYear
+            : `${createdYear}-${currentYear}`}{" "}
+          جميع الحقوق محفوظة.
         </P>
       </Container>
     </footer>
