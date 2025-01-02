535b1fdd1792554d0fa61a750321475f7856fb4b
aissa-dev1 <aissadev1@gmail.com>
2025-01-02 23:49:50 +0100

    create dashboard nav bar

---
+++ src/components/reusable/nav-bar/dashboard.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Container from "../container";
+import { P } from "@/components/ui/typography";
+import ThemeSwitchButton from "../button/theme-switch";
+
+export default function DashboardNavBar() {
+  return (
+    <nav className="fixed left-0 top-0 w-full h-[var(--navbar-height)] flex__center bg-primary text-primary-foreground py-4 shadow-sm">
+      <Container className="flex justify-between items-center px-4">
+        <Link href="/">
+          <P size="lg" className="font-bold">
+            Buildfolio
+          </P>
+        </Link>
+        <div className="flex__center gap-md">
+          <Link href="/">
+            <P className="hover:text-foreground">مرحبا</P>
+          </Link>
+          <ThemeSwitchButton />
+        </div>
+      </Container>
+    </nav>
+  );
+}
